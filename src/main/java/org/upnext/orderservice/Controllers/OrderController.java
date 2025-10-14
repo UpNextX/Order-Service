16f6034a8a70e39087e3c408310dab491cee14fd
Eslam Taha <i777slam73@gmail.com>
2025-10-14 18:01:26 +0300

    Security configs

--- src/main/java/org/upnext/orderservice/Controllers/OrderController.java
+++ src/main/java/org/upnext/orderservice/Controllers/OrderController.java
@@ -4,18 +4,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.upnext.orderservice.Annotations.RequiredRole;
 import org.upnext.orderservice.Services.OrderService;
 import org.upnext.sharedlibrary.Dtos.*;
-import org.upnext.sharedlibrary.Enums.OrderStatus;
 import org.upnext.sharedlibrary.Errors.Result;
 
 import java.net.URI;
 import java.util.List;
-
-import static org.upnext.orderservice.Utils.UserExtractor.userExtractor;
 
 @RestController
 @RequestMapping("/orders")
@@ -27,10 +25,9 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    @RequiredRole("ADMIN")
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
-    public ResponseEntity<?> getOrders(HttpServletRequest request) {
-        UserDto user = userExtractor(request);
+    public ResponseEntity<?> getOrders(HttpServletRequest request, @AuthenticationPrincipal UserDto user) {
 
         Result<List<OrderDto>> result = orderService.getAllOrders();
         if (result.isSuccess()) {
@@ -40,10 +37,9 @@ public class OrderController {
                 .body(result.getError().getMessage());
     }
 
-    @RequiredRole("AUTHENTICATED")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}")
-    public ResponseEntity<?> getOrderById(HttpServletRequest request, @PathVariable Long id) {
-        UserDto user = userExtractor(request);
+    public ResponseEntity<?> getOrderById(@AuthenticationPrincipal UserDto user, @PathVariable Long id) {
 
         Result<OrderDto> result = orderService.getOrderById(user, id);
         if (result.isSuccess()) {
@@ -53,10 +49,9 @@ public class OrderController {
                 .body(result.getError().getMessage());
     }
 
-    @RequiredRole("AUTHENTICATED")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/me")
-    public ResponseEntity<?> getUserOrders(HttpServletRequest request) {
-        UserDto user = userExtractor(request);
+    public ResponseEntity<?> getUserOrders(@AuthenticationPrincipal UserDto user) {
 
         Result<List<OrderDto>> result = orderService.getUserOrders(user.getId());
 
@@ -68,9 +63,9 @@ public class OrderController {
                 .body(result.getError().getMessage());
     }
 
-    @RequiredRole("ADMIN")
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/user/{userId}")
-    public ResponseEntity<?> getUserOrders(HttpServletRequest request, @PathVariable Long userId) {
+    public ResponseEntity<?> getUserOrders(@AuthenticationPrincipal UserDto user, @PathVariable Long userId) {
         Result<List<OrderDto>> result = orderService.getUserOrders(userId);
         if (result.isSuccess()) {
             return ResponseEntity.ok(result.getValue());
@@ -79,10 +74,9 @@ public class OrderController {
                 .body(result.getError().getMessage());
     }
 
-    @RequiredRole("AUTHENTICATED")
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/me/prepare")
-    public ResponseEntity<?> createOrder(@Valid @RequestBody OrderPaymentRequest orderPaymentRequest, HttpServletRequest request, UriComponentsBuilder urb) throws Exception {
-        UserDto user = userExtractor(request);
+    public ResponseEntity<?> createOrder(@Valid @RequestBody OrderPaymentRequest orderPaymentRequest, @AuthenticationPrincipal UserDto user, UriComponentsBuilder urb) throws Exception {
         Result<OrderPaymentResponse> order = orderService.placeOrder(user, orderPaymentRequest, urb);
         if (order.isSuccess()) {
             return ResponseEntity.status(HttpStatus.CREATED).body(order.getValue());
@@ -91,9 +85,9 @@ public class OrderController {
                 .body(order.getError().getMessage());
     }
 
-    @RequiredRole("ADMIN")
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateOrder(@PathVariable Long id, @Valid @RequestBody OrderStatusRequest orderStatusRequest, HttpServletRequest request, UriComponentsBuilder urb) {
+    public ResponseEntity<?> updateOrder(@PathVariable Long id, @Valid @RequestBody OrderStatusRequest orderStatusRequest, @AuthenticationPrincipal UserDto user, UriComponentsBuilder urb) {
         Result<URI> result = orderService.updateOrderStatus(id, orderStatusRequest, urb);
         if(result.isSuccess()) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(result.getValue());
@@ -103,10 +97,9 @@ public class OrderController {
     }
 
 
-    @RequiredRole("AUTHENTICATED")
+    @PreAuthorize("isAuthenticated()")
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> cancelOrder(@PathVariable Long id, HttpServletRequest request, UriComponentsBuilder urb) {
-        UserDto user = userExtractor(request);
+    public ResponseEntity<?> cancelOrder(@PathVariable Long id, @AuthenticationPrincipal UserDto user, UriComponentsBuilder urb) {
 
         Result<URI> result = orderService.cancelOrder(user, id, urb);
         if (result.isSuccess()) {
